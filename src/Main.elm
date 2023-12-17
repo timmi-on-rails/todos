9e4463aa8a066f8b0d8813085b28734933b0ea8e
Tom Scheler <dd-tom@web.de>
2023-12-17 01:21:42 +0100

    add auth ports

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Json.Decode as D
+import Json.Encode as E
 import Persistence
 import Task
 import Todo
@@ -12,10 +14,10 @@ import Todo
 -- MAIN
 
 
-main : Program () Model Msg
+main : Program E.Value Model Msg
 main =
     Browser.element
-        { init = \() -> ( init, Persistence.getTodos |> Task.attempt UpdateTodos )
+        { init = init
         , view = view
         , update = update
         , subscriptions = \_ -> Sub.none
@@ -27,12 +29,52 @@ main =
 
 
 type alias Model =
-    List Todo.Todo
+    { todos : List Todo.Todo
+    , webDavConfig : Maybe WebDavConfig
+    }
 
 
-init : Model
-init =
-    []
+init : E.Value -> ( Model, Cmd Msg )
+init flags =
+    ( let
+        model =
+            { todos = [], webDavConfig = Nothing }
+      in
+      case D.decodeValue decoder flags of
+        Ok cfg ->
+            { model | webDavConfig = Just cfg }
+
+        Err _ ->
+            model
+    , Persistence.getTodos |> Task.attempt UpdateTodos
+    )
+
+
+type alias WebDavConfig =
+    { url : String
+    , user : String
+    , password : String
+    }
+
+
+encode : WebDavConfig -> E.Value
+encode cfg =
+    E.object
+        [ ( "url", E.string cfg.url )
+        , ( "user", E.string cfg.user )
+        , ( "password", E.string cfg.password )
+        ]
+
+
+decoder : D.Decoder WebDavConfig
+decoder =
+    D.map3 WebDavConfig
+        (D.field "url" D.string)
+        (D.field "user" D.string)
+        (D.field "password" D.string)
+
+
+port setWebDavConfig : E.Value -> Cmd msg
 
 
 
@@ -60,7 +102,7 @@ update msg model =
                         Err e ->
                             [ { id = 0, title = e } ]
             in
-            ( s, Cmd.none )
+            ( { model | todos = s }, Cmd.none )
 
 
 
@@ -70,6 +112,17 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick AddTodo ] [ text "add some Todo" ]
-        , div [] (model |> List.map (\todo -> div [] [ text todo.title ]))
+        [ button [ onClick AddTodo ]
+            [ text
+                ("add some Todo "
+                    ++ (model.webDavConfig
+                            |> Maybe.map
+                                (\cfg ->
+                                    cfg.user ++ " " ++ cfg.password ++ " " ++ cfg.url
+                                )
+                            |> Maybe.withDefault ""
+                       )
+                )
+            ]
+        , div [] (model.todos |> List.map (\todo -> div [] [ text todo.title ]))
         ]
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import { Elm } from "./Main.elm";
 
-Elm.Main.init({ node: document.getElementById("root") });
+async function init() {
+    let webDavConfig = {
+        url: localStorage.getItem('webdav-url'),
+        user: "",
+        password: ""
+    };
+
+    try {
+        const credentials = await navigator.credentials.get({ password: true });
+        webDavConfig.user = credentials.id;
+        webDavConfig.password = credentials.password;
+    } catch (e) {
+        console.log(e);
+    }
+
+    const app = Elm.Main.init({
+        node: document.getElementById("root"),
+        flags: webDavConfig
+    });
+
+    app.ports.setWebDavConfig.subscribe(async function (state) {
+        localStorage.setItem('webdav-url', state.url);
+
+        const cred = new PasswordCredential({
+            id: state.user,
+            password: state.password,
+        });
+
+        try {
+            await navigator.credentials.store(cred);
+        } catch (e) {
+            console.log(e);
+        }
+    });
+}
+
+init();
